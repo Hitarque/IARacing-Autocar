70fa7cfc25586a5648ed300b72c94d556d7a1e37
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-05 02:22:13 +0000

    Adding numeric_owner as keyword arguement

--- donkeycar/tests/test_train.py
+++ donkeycar/tests/test_train.py
@@ -89,7 +89,7 @@ def car_dir(tmpdir_factory, base_config, imu_fields) -> str:
                 if not is_within_directory(path, member_path):
                     raise Exception("Attempted Path Traversal in Tar File")
         
-            tar.extractall(path, members, numeric_owner) 
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
             
         
         safe_extract(file, car_dir)
